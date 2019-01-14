420ab64764822e051e4419ac7f4e5ea66b477829
Fraraz87 <F.GIULIANO14@studenti.unisa.it>
2019-01-14 13:58:10 +0100

    Cambiamenti SDD gestione dei dati persistenti

--- InternalWorkProduct/SineChartaDB.sql
+++ InternalWorkProduct/SineChartaDB.sql
@@ -11,7 +11,7 @@ create table utenteRegistrato(
     Nome varchar(50) not null,
     Cognome varchar(50) not null,
     Ruolo enum('utenteModeratore','utenteGiocatore'),
-    primary key(username)
+    primary key(Username)
     );
     
 
@@ -23,47 +23,36 @@ create table Personaggio(
     Nazionalità varchar(20),
     TaroccoDominante varchar(30),
     Caratteristiche int,
-    Abilità varchar(50),
-    Risoluzione int,
-    Ferite char(2),
-    primary key(Nome,Cognome)
+    Abilità varchar(50) not null,
+    Risoluzione int not null,
+    Ferite char(2) not null,
+    Username varchar(15) not null,
+    primary key(Nome,Cognome),
+    foreign key(Username) references utenteRegistrato(Username) on delete cascade
     );
     
 drop table if exists Storia;
 create table Storia(
 	Titolo varchar(50),
     Descrizione varchar(500),
+    Ambientazione enum('Terre Perdute','Quarto Reich','Soviet','Sanctum Imperum'),
     Nome varchar(30),
     Cognome varchar(30),
-    primary key(Nome),
+    primary key(Titolo),
     foreign key(Nome,Cognome) references Personaggio(Nome,Cognome) on delete cascade
     );
 
 drop table if exists Sessione;
 create table Sessione(
 	Numero int default 1,
-    Contenuto varchar(1000),
+    Contenuto varchar(100),
     Username varchar(15),
     primary key(Numero),
     foreign key(Username) references utenteRegistrato(Username) on delete cascade
 	);
     
-drop table if exists Keyword;
-create table Keyword(
-	Chiave varchar(20) not null,
-    descrizione varchar(1000),
-    Numero int default 1,
-    primary key(Chiave),
-    foreign key(Numero) references Sessione(Numero) on delete cascade
-);
 
-drop table if exists Nemico;
-create table Nemico(
-	Nome varchar(30),
-    Attacco varchar(30),
-    Speciali varchar(30),
-    primary key(Nome)
-);
+
 
 drop table if exists Oggetti;
 create table Oggetti(
@@ -89,16 +78,16 @@ create table Armi(
 drop table if exists Realizza;
 create table Realizza(
 	Username varchar(15) not null,
-    Nome varchar(30),
-    primary key(Username, Nome),
+    Titolo varchar(30),
+    primary key(Username, Titolo),
     foreign key(Username) references utenteRegistrato(Username),
-    foreign key(Nome) references Storia(Nome)
+    foreign key(Titolo) references Storia(Titolo)
     );
     
 drop table if exists Crea;
 create table Crea(
 	Username varchar(15) not null,
-    Numero varchar(30),
+    Numero int,
     primary key(Username, Numero),
     foreign key(Username) references utenteRegistrato(Username),
     foreign key(Numero) references Sessione(Numero)
