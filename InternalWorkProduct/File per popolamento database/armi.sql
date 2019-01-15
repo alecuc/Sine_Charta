c1ce327cbd397caa255322795040476e4dea9ddc
aleXamun <alecuccurullo@gmail.com>
2019-01-15 18:04:42 +0100

    Creati file per popolamento del database. L'ordine dei parametri è specificato in testa al docuemtno .sql

---
+++ InternalWorkProduct/File per popolamento database/armi.sql
@@ -0,0 +1,10 @@
+--Le informazioni caricate, in ordine: tipo(Pistola, Mitra, Fucile), modello, danno, munizione, ricarica
+Pistola	Beretta 1934	1	9mm	2
+Pistola	Walther PPK	0	7.65mm	2
+Pistola	Revolver cal.38	1	38	1
+Pistola	Bodeo 1889	2	10.4mm	2
+Mitra	PPHS	0	7.62 TT	7
+Mitra	Thompson	2	45 ACP	5
+Fucile	Doppietta	3	12	1
+Fucile	Carbin M1	1	30 US	2
+Fucile	Winchester M97	3	12	1
