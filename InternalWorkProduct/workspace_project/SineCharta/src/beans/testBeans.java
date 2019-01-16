e5965504351f2b83dcbf2fe4a3d59aefa587ed89
RaffoV7 <af-5@hotmail.it>
2019-01-16 11:05:12 +0100

    Test abilità personaggio

---
+++ InternalWorkProduct/workspace_project/SineCharta/src/beans/testBeans.java
@@ -0,0 +1,18 @@
+package beans;
+
+
+public class testBeans {
+
+	public static void main(String[] args) {
+		
+		Personaggio pg = new Personaggio();
+		
+		pg.setAbility("fuoco", 5);
+		pg.getAbility();
+		System.out.println("klsdjhvleb");
+		
+		
+		
+	}
+
+}
