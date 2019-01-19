a31ee0e94ba6edd0f5ef3ac71e4f1744c2ee0ea1
Fraraz87 <F.GIULIANO14@studenti.unisa.it>
2019-01-20 00:05:19 +0100

    inizato mazzo

--- InternalWorkProduct/workspace_project/SineCharta/src/beans/Mazzo.java
+++ InternalWorkProduct/workspace_project/SineCharta/src/beans/Mazzo.java
@@ -1,9 +1,16 @@
 package beans;
 
+import java.util.ArrayList;
+
 import interfaces.Deckable;
 
 public class Mazzo implements Deckable{
 
+	public ArrayList<CartaPoker> MazzoPoker;
+	public ArrayList<Tarocco> MazzoTarocco;
+	public Mazzo() {
+		
+	}
 	@Override
 	public Object estrai() {
 	
