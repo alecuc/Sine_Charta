f53245b2e16a9a601a7b84852fc0564a0a57451f
RaffoV7 <af-5@hotmail.it>
2019-01-21 11:48:03 +0100

    Aggiunta cartella progetto

---
+++ codice/SineCharta/WebContent/jsp_page/loginForm.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="col mx-auto">
+				<h5 class="card-title text-center">Login</h5>
+				<form class="form-signin" method="post" action="../LoginServlet">
+					<div class="row">
+						<div class="col-md-3 field-label-responsive">
+							<label for="username">Username:</label>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<div class="input-group mb-2 mr-sm-2 mb-sm-0">
+									<div class="input-group-addon" style="width: 2.6rem">
+										<i class="fa fa-user"></i>
+									</div>
+									<input type="text" name="username" class="form-control"
+										id="username" placeholder="Username" required autofocus>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3 field-label-responsive">
+							<label for="password">Password</label>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group has-danger">
+								<div class="input-group mb-2 mr-sm-2 mb-sm-0">
+									<div class="input-group-addon" style="width: 2.6rem">
+										<i class="fa fa-key"></i>
+									</div>
+									<input type="password" name="password" class="form-control"
+										id="password" placeholder="Password" required>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="custom-control custom-checkbox mb-3">
+						<input type="checkbox" class="custom-control-input"
+							id="customCheck1"> <label class="custom-control-label"
+							for="customCheck1">Ricordami</label>
+					</div>
+					<div class="text-center">
+					<button type="submit" class="btn btn-success"
+						style="background-color: #212529; border-color: red;">Login</button>
+					</div>
+				</form>
+				<br> <a href="homeUser.jsp">pagina utente (link provvisorio)</a>
+			</div>
+</body>
+</html>
---
+++ codice/SineCharta/src/control/utente/LoginServlet.java
@@ -0,0 +1,97 @@
+package control.utente;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import manager.UsersManager;
+import beans.User;
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	static UsersManager user = new UsersManager();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		String usernameInput = request.getParameter("username");
+		String passwordInput = request.getParameter("password");
+		HttpSession session = request.getSession();
+		
+		
+		try {
+			User utenteLogin= user.doRetrieveByKey(usernameInput);
+			
+			String password= utenteLogin.getPassword();
+
+			/*if (utenteLogin.getUsername()==null) {
+				response.sendRedirect("jsp_page/login404.jsp");
+			}*/
+			
+			
+				String passEncr = passwordInput;
+				if (passwordInput != null) {
+				    MessageDigest md = MessageDigest.getInstance("MD5"); 
+				    md.update(passwordInput.getBytes());
+				    BigInteger hash = new BigInteger(1, md.digest());
+				    passEncr = hash.toString(16);
+				    while (passEncr.length() < 32) {
+				    	passEncr = "0" + passEncr;
+				    }
+				}	
+				
+				
+			if (passEncr.equals(password)) {
+				session.setAttribute("username", usernameInput);
+				/*
+				 * TODO: METTERE COME ATTRIBUTO DI SESSIONE:
+				 * -STORIE A CUI PARTECIPO
+				 */
+				response.sendRedirect("jsp_page/homeUser.jsp");
+			} else {
+			
+			System.out.println("OH NON SONO UGUALI LE DUE PASSWORD");
+			response.sendRedirect("jsp_page/loginErrorePassword.jsp");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
