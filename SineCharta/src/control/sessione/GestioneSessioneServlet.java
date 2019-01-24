f1849d3eb36b839e7f2f370c95cc000e659ec494
Fraraz87 <F.GIULIANO14@studenti.unisa.it>
2019-01-24 18:06:38 +0100

    Iniziate le servlet, da finire quando i manager sono terminati

--- SineCharta/src/control/sessione/GestioneSessioneServlet.java
+++ SineCharta/src/control/sessione/GestioneSessioneServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.SessioneDiGioco;
+import manager.SessioneManager;
 
 /**
  * Servlet implementation class GestioneSessioneServlet
@@ -13,28 +17,71 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/GestioneSessioneServlet")
 public class GestioneSessioneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public GestioneSessioneServlet() {
         super();
-        // TODO Auto-generated constructor stub
+     
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		String action = request.getParameter("action");
+		SessioneDiGioco sesDiGioco = new SessioneDiGioco();
+		SessioneManager ssn = new SessioneManager();
+		HttpSession session = request.getSession();
+		
+		try {
+			
+			if(action.equalsIgnoreCase("inserisciSessione")) {
+				
+				String contenuto = request.getParameter("contenutoSessione");
+				String username = request.getParameter("Username");
+				String idStory = request.getParameter("idStoria");
+				
+				Integer idStor = Integer.parseInt(idStory);
+				
+				sesDiGioco.setContenutoSessione(contenuto);
+				sesDiGioco.setUsernameModeratore(username);
+				sesDiGioco.setIdStoria(idStor);
+				
+				//ssn.salvareSessioni(sesDiGioco);		
+				
+				System.out.println("successfully inserted");
+				response.sendRedirect("jps_page/vistaSessione.jsp");
+			}else if(action.equalsIgnoreCase("listaSessione")) {
+				//logica per lista delle sessioni
+			}else if(action.equalsIgnoreCase("modificaSessione")) {
+				
+				String idSessione = request.getParameter("idSessione");
+				String contenuto = request.getParameter("contenutoSessione");
+				String username = (String)session.getAttribute("Username");
+				String idStoria = (String)session.getAttribute("idStor");
+				
+				Integer id = Integer.parseInt(idStoria);
+				
+				//chiamare il manager per modificare il contenuto della sessione di gioco
+				
+				
+			}
+		}finally {
+			
+		}/* catch(SQLException e) {
+			e.printStackTrace();
+		}*/
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
