377525df3a27305992b8ac7d59cddc195e8cc853
Raffaele Vitiello <af-5@hotmail.it>
2019-02-03 16:55:02 +0100

    aggiunto nuovo connectore e aggiustamenti test

---
+++ SineCharta/test/manager/UsersManagerTest2.java
@@ -0,0 +1,34 @@
+package manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UsersManagerTest2 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoRetrieveByKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoRetrieveAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckUser() {
+		fail("Not yet implemented");
+	}
+
+}
